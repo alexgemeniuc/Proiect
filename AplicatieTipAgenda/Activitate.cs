c2851a1455afc9f3c84363606f449ffb3ebc3c27
alexgemeniuc <gemeniucalexandru@gmail.com>
2025-03-09 21:56:15 +0200

    Add files via upload

---
+++ AplicatieTipAgenda/Activitate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AplicatieTipAgenda
+{
+    public class Activitate
+    {
+        public string Titlu { get; set; }
+        public string Descriere { get; set; }
+        public DateTime Data { get; set; }
+        public string TipActivitate { get; set; }
+
+        public Activitate(string titlu, string descriere, DateTime data, string tipActivitate)
+        {
+            Titlu = titlu;
+            Descriere = descriere;
+            Data = data;
+            TipActivitate = tipActivitate;
+        }
+
+        public override string ToString()
+        {
+            return $"[{Data:dd/MM/yyyy HH:mm}] {Titlu} - {TipActivitate}: {Descriere}";
+        }
+    }
+}
---
+++ AplicatieTipAgenda/Agenda.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AplicatieTipAgenda
+{
+    public class Agenda
+    {
+        private List<Activitate> activitati;
+
+        public Agenda()
+        {
+            activitati = new List<Activitate>();
+        }
+
+        public void AdaugaActivitate(Activitate activitate)
+        {
+            activitati.Add(activitate);
+        }
+
+        public void StergeActivitate(string titlu)
+        {
+            activitati.RemoveAll(a => a.Titlu.Equals(titlu, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public void AfiseazaActivitatiDinZi(DateTime data)
+        {
+            var activitatiDinZi = activitati.Where(a => a.Data.Date == data.Date).ToList();
+            if (activitatiDinZi.Count == 0)
+            {
+                Console.WriteLine("Nu există activități pentru această zi.");
+                return;
+            }
+            activitatiDinZi.ForEach(Console.WriteLine);
+        }
+
+        public void AfiseazaActivitatiDupaTip(string tip)
+        {
+            var activitatiTip = activitati.Where(a => a.TipActivitate.Equals(tip, StringComparison.OrdinalIgnoreCase)).ToList();
+            if (activitatiTip.Count == 0)
+            {
+                Console.WriteLine($"Nu există activități de tipul \"{tip}\".");
+                return;
+            }
+            activitatiTip.ForEach(Console.WriteLine);
+        }
+
+        public void AfiseazaActivitatiRepetate()
+        {
+            var grupate = activitati.GroupBy(a => a.Titlu)
+                                    .Where(g => g.Count() > 1)
+                                    .ToList();
+
+            if (grupate.Count == 0)
+            {
+                Console.WriteLine("Nu există activități repetate.");
+                return;
+            }
+
+            foreach (var grup in grupate)
+            {
+                Console.WriteLine($"Activitatea \"{grup.Key}\" apare de {grup.Count()} ori:");
+                foreach (var act in grup)
+                    Console.WriteLine($"\t{act}");
+            }
+        }
+    }
+}
